a0ec7711837b5690dfbb7e0712250a42505724ec
andraaae <andrariedwan@gmail.com>
2025-10-07 15:11:01 +0700

    tugas2.dart

---
+++ tugas2.dart
@@ -0,0 +1,11 @@
+import "dart:io";
+
+void main() {
+
+  stdout.write("masukan nilai uts siswa");
+String? inputNilai = stdin.readLineSync();
+  stdout.write("masukan nilai uas siswa");
+  String? inputNilai = stdin.readLineSync();
+
+  
+}
