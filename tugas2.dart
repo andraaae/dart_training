f6f382c1cbf07a08447dc2f4165a146ee339987c
andraaae <andrariedwan@gmail.com>
2025-10-08 08:48:27 +0700

    tugas 2

--- tugas2.dart
+++ tugas2.dart
@@ -2,10 +2,47 @@ import "dart:io";
 
 void main() {
 
-  stdout.write("masukan nilai uts siswa");
-String? inputNilai = stdin.readLineSync();
-  stdout.write("masukan nilai uas siswa");
-  String? inputNilai = stdin.readLineSync();
+ //input nilai UTS
+ stdout.write("masukan nilai UTS siswa");
+ String? inputnilaiUTS = stdin.readLineSync();
 
+ int nilaiUTS = int.parse(inputnilaiUTS ?? "0");
+
+ //input nilai UAS
+ stdout.write("masukan nilai UAS");
+String? inputnilaiUAS = stdin.readLineSync();
+
+ int nilaiUAS = int.parse(inputnilaiUAS ?? "0");
+
+ //input kehadiran
+ stdout.write("masukan kehadiran (0-100)");
+ String? persenKehadiran = stdin.readLineSync();
+ double kehadiran = double.parse(persenKehadiran ?? "0");
+
+var nilaiRata = (nilaiUTS+nilaiUAS)/2;
+
+print("NilaiUTS: $nilaiUTS");
+print("NilaiUAS: $nilaiUAS");
+print("kehadiran: $kehadiran");
+
+if (nilaiRata >= 70 && kehadiran >= 0.75 && nilaiUTS >= 60 && nilaiUAS >= 60){
+     print ("Selamat! Anda dinyatakan LULUS");
+
+} else if (nilaiRata < 70 && kehadiran >= 0.75 && nilaiUTS >= 60 && nilaiUAS >= 60){
+    print ("Mohon maaf, Anda BELUM dinyatakan LULUS"); 
+
+}else if (nilaiRata >= 70 && kehadiran < 0.75 && nilaiUTS >= 60 && nilaiUAS >= 60){
+  print ("Mohon maaf, Anda BELUM dinyatakan LULUS");
+
+}else if (nilaiRata >= 70 && kehadiran >= 0.75 && nilaiUTS < 60 && nilaiUAS >= 60){
+    print ("Mohon maaf Anda BELUM dinyatakan LULUS");
   
+}else if (nilaiRata >= 70 && kehadiran >= 0.75 && nilaiUTS >= 60 && nilaiUAS < 60){
+    print ("Mohon maaf, Anda BELUM dinyatakan LULUS");
+
+}else (){
+    print ("Tidak Lulus"); 
+  };
 }
+
+
